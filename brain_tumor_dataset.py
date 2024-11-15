cc756e75a2548940ec8fe6f10d1e7cb727f7fcd7
Andreas691667 <andreaskaag@hotmail.com>
2024-11-15 10:01:18 +0100

    refactor models into different files

--- brain_tumor_dataset.py
+++ brain_tumor_dataset.py
@@ -5,6 +5,7 @@ import numpy as np
 from torch.utils.data import Dataset
 import kagglehub
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 TEST_DATA_PATH = os.path.join(
@@ -54,3 +55,23 @@ class BrainTumorDataset(Dataset):
         label = self.class_to_idx[label] if isinstance(label, str) else int(label)
 
         return img, label
+
+
+# Function to show a grid of images from a single batch
+def show_image_grid(images, labels, rows=4, cols=8):
+    fig, axes = plt.subplots(rows, cols, figsize=(15, 10))
+    axes = axes.flatten()
+    
+    # Display each image in the batch
+    for i in range(len(images)):
+        img = images[i].numpy().transpose((1, 2, 0))  # Convert to HWC format for plotting
+        axes[i].imshow(img, cmap="gray")
+        axes[i].set_title(f"Label: {labels[i]}")
+        axes[i].axis("off")  # Hide axes
+
+    # Hide any extra subplots in case there are fewer images than grid cells
+    for j in range(len(images), len(axes)):
+        axes[j].axis("off")
+
+    plt.tight_layout()
+    plt.show()
